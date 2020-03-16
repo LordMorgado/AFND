de010f7521ec85937d66d48d0b9d4798bbeff8ec
LordMorgado <arturomorgado117@gmail.com>
2020-03-16 13:06:27 -0600

    analizador léxico completo

--- practica1.py
+++ practica1.py
@@ -1,5 +1,7 @@
 from os import system
 from graphviz import Digraph
+from automata import *
+from lexico import *
 
 def dibujaAutomata(afnd):
     f = Digraph('finite_state_machine', filename='afnd.gv')
@@ -16,232 +18,7 @@ def dibujaAutomata(afnd):
             f.edge(str(estado.id), str(trs.estadoDestino.id), label=trs.simbolo)
     f.view()
 
-class Transicion:
-    def __init__(self, simbolo=None, estadoDestino=None):
-        if simbolo is None:
-            self.simbolo = None
-        else:
-            self.simbolo = simbolo
-        
-        if estadoDestino is None:
-            self.estadoDestino = None
-        else:
-            self.estadoDestino = estadoDestino
 
-class Estado:
-    _id = 0
-    def __init__(self, transiciones=None, simboloAFD=None):
-        self.id = Estado._id
-        if transiciones is None:
-            self.transiciones = []
-        else:
-            self.transiciones = transiciones
-        if simboloAFD is None:
-            self.simboloAFD = ""
-        else:
-            self.simboloAFD = simboloAFD
-        Estado._id = Estado._id + 1
-        self.aceptacion = False
-        
-    def acepta(self):
-        if self.aceptacion:
-            self.aceptacion = False
-        else:
-            self.aceptacion = True
-
-class Automata:
-    def __init__(self, simbolo=None):
-        self.edosceptacion = []
-        self.edosAutomata = []
-        self.kleene = False
-        self.positiva = False
-        self.signo = False
-        self.afd = False
-        if simbolo is None:
-            self.edoInicial = Estado()
-            self.alfabeto = ["ε"]
-        else:
-            self.alfabeto = ["ε", simbolo]
-            self.edoInicial = Estado()
-            estadoAceptacion = Estado()
-            estadoAceptacion.acepta()
-            self.edosceptacion.append(estadoAceptacion)
-            self.edosAutomata = [self.edoInicial, estadoAceptacion]
-            self.edoInicial.transiciones.append(Transicion(simbolo, estadoAceptacion))
-    
-    def unirAutomata(self, automata):
-        e1 = Estado()
-        e2 = Estado()
-        e1.transiciones.append(Transicion("ε", self.edoInicial))
-        e1.transiciones.append(Transicion("ε", automata.edoInicial))
-        for estTran in self.edosceptacion:
-          estTran.transiciones.append(Transicion("ε", e2))
-          estTran.acepta()
-
-        for estTran2 in automata.edosceptacion:
-          estTran2.transiciones.append(Transicion("ε", e2))
-          estTran2.acepta()
-        e2.acepta()
-
-        self.alfabeto = list(set(self.alfabeto) | set(automata.alfabeto) )
-        self.edosAutomata = list(set(self.edosAutomata) | set(automata.edosAutomata) )
-        self.edosAutomata.append(e1)
-        self.edosAutomata.append(e2)
-        self.edosceptacion = []
-        self.edosceptacion.append(e2)
-        self.edoInicial = e1
-        automata = None
-        return self
-    
-    def concatena(self, automata):
-
-        for e in self.edosceptacion:
-            e.transiciones = list(set(e.transiciones) | set(automata.edoInicial.transiciones) )
-            e.acepta()
-        automata.edoInicial.transiciones = []
-        self.alfabeto = list(set(self.alfabeto) | set(automata.alfabeto) )
-        self.edosceptacion = []
-        self.edosceptacion = automata.edosceptacion
-        self.edosAutomata = list(set(self.edosAutomata) | set(automata.edosAutomata) )
-        self.edosAutomata.remove(automata.edoInicial)
-        automata = None
-        return self
-
-    def cerraduraPositiva(self):
-        if not self.kleene and not self.signo:
-            self.positiva = True
-            en1 = Estado()
-            en2 = Estado()
-            eAux = self.edoInicial
-
-            for e in self.edosceptacion:
-                e.transiciones.append(Transicion("ε", en1))
-                e.acepta()
-                e.transiciones.append(Transicion("ε", self.edoInicial))
-            self.edosceptacion = []
-            en1.acepta()
-            self.edosceptacion.append(en1)
-            self.edosAutomata.append(en1)
-
-            self.edosAutomata.remove(self.edoInicial)
-            self.edosAutomata.append(eAux)
-            en2.transiciones.append(Transicion("ε", eAux))
-            self.edoInicial = en2
-
-            for e in self.edosAutomata:
-                if e.id == 0:
-                    e.id = self.edoInicial.id
-                    self.edoInicial.id = 0
-                    break
-
-
-            self.edosAutomata.append(self.edoInicial)
-            
-            
-        return self
-
-    def cerraduraKleene(self):
-        if not self.positiva and not self.signo:
-            self.cerraduraPositiva()
-            for e in self.edosceptacion:
-                self.edoInicial.transiciones.append(Transicion("ε", e))
-            self.kleene = True
-        return self
-
-    def cerraduraSigno(self):
-        if not self.kleene and not self.positiva:
-            for e in self.edosceptacion:
-                self.edoInicial.transiciones.append(Transicion("ε", e))
-            self.signo = True
-
-
-def mover(conjunto, simbolo):
-    R = []
-    for e in conjunto:
-        for t in e.transiciones:
-            if t.simbolo == simbolo:
-                R.append(t.estadoDestino)
-    return R
-
-def cerraduraE (e: Estado):
-    R = []
-    pila = []
-    pila.append(e)
-    while pila:
-        e2 = pila.pop()
-        if e2 in R:
-            continue
-        R.append(e2)
-        for trn in e2.transiciones:
-            if trn.simbolo == "ε":
-                if not (trn.estadoDestino in R):
-                    pila.append(trn.estadoDestino)
-    return R
-    
-def cerraduraEestados(conjunto):
-    R = []
-    for e in conjunto:
-        R = list(set(R) | set(cerraduraE(e)))
-    return R
-
-def goTo (stados, simbolo):
-        return cerraduraEestados(mover(stados, simbolo));
-
-def afnToafd(afn: Automata):
-
-    afd = Automata()
-    estadoInicial = Estado()
-    estadoInicial.id = 0
-    Sn = [] #new Set<State>();
-    estadoAProcesar = [] #new Set<State>();
-    conjuntoA = [] #new Set<Set<State>>(); //Es un conjunto de conjuntos de conjuntoA
-    conjuntoAAFD = []#new Set<State>(); //Contendra conjuntoA numerados de 0 a n
-    transicion = Transicion()
-    state =  Estado()
-
-    queueA = []
-    conjuntoA.clear()
-
-    afd.alfabeto = afn.alfabeto # Se copia el alfabeto del AFN al AFD
-    afd.alfabeto.remove("ε") # Elimina Epsilon del alfabeto perteneciente al AFD
-    Sn = cerraduraE(afn.edoInicial) # Se calcula la cerradura epsilon del estado inicial y se guarda en Sn
-    for s in Sn:
-        if s in afn.edosceptacion:
-            estadoInicial.acepta()
-            afd.edosceptacion.append(estadoInicial)
-            break
-    queueA.append(Sn)
-    conjuntoA.append(Sn)
-    afd.edosAutomata.append(estadoInicial) # Agrega el inicial
-    afd.edoInicial = estadoInicial
-
-    
-
-        
-    while len(queueA) != 0:
-        estadoAProcesar = queueA[0] # quita de cola
-        for simb in afd.alfabeto:
-            Sn = goTo(estadoAProcesar, simb) # representa la S
-            if not (Sn in conjuntoA) and Sn != []: # Si no exisitia este subconjunto de conjuntoA va a conformar un nuevo estado del AFD con su transicion
-                state = Estado()
-                queueA.append(Sn) # se va a analizar el siguiente
-                conjuntoA.append(Sn)
-                transicion = Transicion(simb, state)
-                for estadosDeSn in Sn:
-                    if estadosDeSn in afn.edosceptacion: #Si el conjunto contiene al menos un estado de aceptacion
-                        state.acepta()
-                        afd.edosceptacion.append(state)
-                        break
-
-                afd.edosAutomata[conjuntoA.index(estadoAProcesar)].transiciones.append(transicion)
-                afd.edosAutomata.append(state)
-            elif Sn != []:
-                transicion = Transicion(simb, afd.edosAutomata[conjuntoA.index(Sn)])
-                afd.edosAutomata[conjuntoA.index(estadoAProcesar)].transiciones.append(transicion)
-
-        queueA.pop(0)
-    
-    return afd
 
 ans = True
 automatas = []
@@ -256,7 +33,10 @@ while ans:
     6.cerradura signo '?'
     7.dibuja autómata
     8.pasar a AFD
-    9.Exit/Quit
+    9.análisis léxico
+    10.Indicar clase léxica de automata
+    11.duplicar automata
+    12.Exit/Quit
     """)
     ans = input("elige una opción ")
 
@@ -266,6 +46,8 @@ while ans:
         automatas.append(Automata(simboloNuevoAlfabeto))
     
     elif ans=="2":
+        if len(automatas) < 2:
+            continue
         print("\n Unir dos autómatas")
         print("Selecciona los autómatas a unir:\n")
         for i in range(0,len(automatas)):
@@ -287,6 +69,8 @@ while ans:
         #automatas[0].unirAutomata(automatas[1])
     
     elif ans=="3":
+        if len(automatas) < 2:
+            continue
         print("\n Concatenar dos automatas")
         print("Selecciona los autómatas a concatenar:\n")
         for i in range(0,len(automatas)):
@@ -342,6 +126,8 @@ while ans:
                 print("Opción no son válida")
 
     elif ans=="7":
+        if len(automatas) == 0:
+            continue
         for i in range(0,len(automatas)):
             print("automata " + str(i+1) + " = " + str(automatas[i].alfabeto))
 
@@ -354,6 +140,8 @@ while ans:
                 print("Opción no son válida")
 
     elif ans=="8":
+        if len(automatas) == 0:
+            continue
         for i in range(0,len(automatas)):
             print("automata " + str(i+1) + " = " + str(automatas[i].alfabeto))
 
@@ -366,7 +154,53 @@ while ans:
                 print("Opción no son válida")
 
     elif ans=="9":
-        print("\n Adios")
+        if len(automatas) == 0:
+            continue
+        print("Elige el automata")
+        for i in range(0,len(automatas)):
+            print("automata " + str(i+1) + " = " + str(automatas[i].alfabeto))
+        while True:
+            opc1 = int(input())
+            if opc1 in range(1,len(automatas)+1):
+                automatas[opc1-1].tablaDeTransiciones()
+                cadena = input("Introduce la cadena a analizar: ")
+                LexicAnalyzer(automatas[opc1-1], cadena).getToken()
+                s = input("esperando...")
+                break
+            else:
+                print("Opción no son válida")
+
+    elif ans=="10":
+        if len(automatas) == 0:
+            continue
+        print("Elige el automata")
+        for i in range(0,len(automatas)):
+            print("automata " + str(i+1) + " = " + str(automatas[i].alfabeto))
+        while True:
+            opc1 = int(input())
+            if opc1 in range(1,len(automatas)+1):
+                cadena = input("Introduce El nombre de la clase léxica: ")
+                automatas[opc1-1].setClaseLexica(cadena)
+                break
+            else:
+                print("Opción no son válida")
+
+    elif ans=="11":
+        if len(automatas) == 0:
+            continue
+        print("Elige el automata a Duplicar")
+        for i in range(0,len(automatas)):
+            print("automata " + str(i+1) + " = " + str(automatas[i].alfabeto))
+        while True:
+            opc1 = int(input())
+            if opc1 in range(1,len(automatas)+1):
+                automatas.append(automatas[opc1-1].duplicar())
+                break
+            else:
+                print("Opción no son válida")
+    elif ans=="12":
+        print("\n Adeu")
         ans = None
+    
     else:
         print("\n Esa no es una opción")
